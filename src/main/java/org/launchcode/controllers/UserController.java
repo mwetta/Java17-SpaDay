e60bc204e4cbc840da5b25d76318b7c837344824
Madeline <moon.madeline@gmail.com>
2023-10-09 19:02:13 -0500

    Spa Day Studio Pt 2

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,2 +1,24 @@
-package org.launchcode.controllers;public class UserController {
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping("add")
+    public String displayAddUserForm(String username, String email, String password, String verify) {
+        User user = new User(username, email, password, verify);
+    }
+
+//    @PostMapping("/signup")
+//    public String processSignup(@RequestParam String fName, String lName, String email){
+//        User user = new User(fName, lName, email);
+//        users.add(user);
+//        return "redirect";
+//    }
 }
+
